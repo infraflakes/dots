b64da444064c7453d0deb8c7abd43cc62dbbb78d
infraflakes <infraflakes@proton.me>
2026-06-01 16:18:41 +0700

    OSC 52

--- config/nvim/lua/serein/options.lua
+++ config/nvim/lua/serein/options.lua
@@ -6,10 +6,18 @@ vim.o.number = true
 vim.o.mouse = "a"
 vim.o.showmode = true
 
-vim.schedule(function()
-  vim.o.clipboard = "unnamedplus"
-end)
-
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+    ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+    ["*"] = require("vim.ui.clipboard.osc52").paste "*",
+  },
+}
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
