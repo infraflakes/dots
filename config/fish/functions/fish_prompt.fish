b9dc413d20a559a6dc8844f74dcbd9ba030a1bc2
infraflakes <infraflakes@proton.me>
2026-07-28 13:47:20 +0700

    Fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,23 +1,18 @@
 function fish_prompt
     echo
-    # Left pill cap (Flexoki light gray background color)
-    set_color -o e6e4de
+    set_color -o 4a4b52
     echo -n ""
     set_color normal
-    # Icon (Flexoki Black text on light gray background)
-    set_color -o 100f0f
-    set_color -b e6e4de
+    set_color -o 8ab4f8
+    set_color -b 4a4b52
     echo -n "  "
     set_color normal
-    # Path (Flexoki Black text on light gray background)
-    set_color -o 100f0f
-    set_color -b e6e4de
+    set_color -o 8ab4f8
+    set_color -b 4a4b52
     echo -n (prompt_pwd)
     set_color normal
-    # Right pill cap (Flexoki light gray background color)
-    set_color -o e6e4de
+    set_color -o 4a4b52
     echo -n ""
     set_color normal
-    # Prompt arrow (Flexoki Black)
-    echo -n (set_color -o 100f0f)" ❯ "
+    echo -n (set_color -o 8ab4f8)" ❯ "
 end
